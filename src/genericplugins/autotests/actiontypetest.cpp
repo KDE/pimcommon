ce2adfd812112c15f7eaf802a5fb7b816e4994ee
Montel Laurent <montel@kde.org>
2015-12-14 23:05:43 +0100

    add autotest

---
+++ src/genericplugins/autotests/actiontypetest.cpp
@@ -0,0 +1,51 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include "actiontypetest.h"
+#include "../genericplugininterface.h"
+#include <QAction>
+#include <QTest>
+
+ActionTypeTest::ActionTypeTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+ActionTypeTest::~ActionTypeTest()
+{
+
+}
+
+void ActionTypeTest::shouldHaveDefaultValue()
+{
+    PimCommon::ActionType actionType;
+    QVERIFY(!actionType.action());
+    QCOMPARE(actionType.type(), PimCommon::ActionType::Tools);
+}
+
+void ActionTypeTest::shouldAssignValue()
+{
+    QAction *act = new QAction(Q_NULLPTR);
+    PimCommon::ActionType::Type type = PimCommon::ActionType::File;
+    PimCommon::ActionType actionType(act, type);
+    QCOMPARE(actionType.action(), act);
+    QCOMPARE(actionType.type(), type);
+}
+
+QTEST_MAIN(ActionTypeTest)
