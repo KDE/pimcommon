cfecd5a663e620faeb509de0a27cab5ba187913f
Volker Krause <vkrause@kde.org>
2018-10-28 12:21:09 +0100

    License maintenance: GPLv2 -> GPLv2+
    Summary:
    This should now cover all remaining GPLv2-only files in pimcommon,
    excluding test code. This is based on existing approval from
    https://techbase.kde.org/Projects/KDE_Relicensing.
    
    Reviewers: mlaurent
    
    Reviewed By: mlaurent
    
    Subscribers: kde-pim
    
    Tags: #kde_pim
    
    Differential Revision: https://phabricator.kde.org/D16477

--- src/pimcommonakonadi/acl/collectionaclpage.h
+++ src/pimcommonakonadi/acl/collectionaclpage.h
@@ -4,9 +4,10 @@
  * Copyright (c) 2004 David Faure <faure@kde.org>
  *
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; version 2 of the License
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
