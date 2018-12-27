3d771fb4c7811f6ac0e6129f3d2df7ef2d6cba1d
Laurent Montel <montel@kde.org>
2018-12-27 18:34:12 +0100

    GIT_SILENT: Update copyright

--- src/pimcommon/autocorrection/autocorrection.cpp
+++ src/pimcommon/autocorrection/autocorrection.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2012-2018 Montel Laurent <montel@kde.org>
+  Copyright (c) 2012-2019 Montel Laurent <montel@kde.org>
   code based on calligra autocorrection.
 
   This program is free software; you can redistribute it and/or modify it
