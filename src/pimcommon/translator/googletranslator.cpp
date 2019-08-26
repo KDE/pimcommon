4208603882fb9725357a5d95898cdb1ced2289f4
Laurent Montel <montel@kde.org>
2019-08-26 08:04:25 +0200

    Use QLatin1String in join/startswith/endsWith/contains + QString::replace operates on the same object, no need to reassign

--- src/pimcommon/translator/googletranslator.cpp
+++ src/pimcommon/translator/googletranslator.cpp
@@ -188,8 +188,8 @@ void GoogleTranslator::slotTranslateFinished(QNetworkReply *reply)
     reply->deleteLater();
     //  jsonData contains arrays like this: ["foo",,"bar"]
     //  but this is not valid JSON for QJSON, it expects empty strings: ["foo","","bar"]
-    mJsonData = mJsonData.replace(QRegExp(QLatin1String(",{3,3}")), QStringLiteral(",\"\",\"\","));
-    mJsonData = mJsonData.replace(QRegExp(QLatin1String(",{2,2}")), QStringLiteral(",\"\","));
+    mJsonData.replace(QRegExp(QLatin1String(",{3,3}")), QStringLiteral(",\"\",\"\","));
+    mJsonData.replace(QRegExp(QLatin1String(",{2,2}")), QStringLiteral(",\"\","));
     //qCDebug(PIMCOMMON_LOG) << mJsonData;
 
     QJsonParseError parsingError;
