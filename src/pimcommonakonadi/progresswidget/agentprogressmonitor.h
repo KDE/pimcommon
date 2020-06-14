a4d7a0d1b0ae6081e33c23dfdfc697cca44db49d
Laurent Montel <montel@kde.org>
2020-06-14 17:09:39 +0200

    Move progressmanagerakonadi from libkdepim

---
+++ src/pimcommonakonadi/progresswidget/agentprogressmonitor.h
@@ -0,0 +1,54 @@
+/*
+    Copyright (c) 2009 Constantin Berzan <exit3219@gmail.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef PIMCOMMON_AGENTPROGRESSMONITOR_H
+#define PIMCOMMON_AGENTPROGRESSMONITOR_H
+
+#include <Libkdepim/ProgressManager> // ProgressItem
+
+#include <AgentInstance>
+namespace PimCommon {
+/**
+ * @internal
+ *
+ * This class automatically updates a ProgressItem based on
+ * Akonadi::AgentManager's signals, and places the abort() call if the
+ * ProgressItem has been cancelled.
+ */
+class AgentProgressMonitor : public QObject
+{
+    Q_OBJECT
+public:
+    // used by our friend ProgressManager
+    AgentProgressMonitor(const Akonadi::AgentInstance &mAgent, KPIM::ProgressItem *mItem);
+    ~AgentProgressMonitor();
+
+private:
+    void abort();
+    void instanceProgressChanged(const Akonadi::AgentInstance &);
+    void instanceStatusChanged(const Akonadi::AgentInstance &);
+    void instanceRemoved(const Akonadi::AgentInstance &);
+    void instanceNameChanged(const Akonadi::AgentInstance &);
+
+    Akonadi::AgentInstance mAgent;
+    QPointer<KPIM::ProgressItem> const mItem;
+};
+}
+
+#endif
---
+++ src/pimcommonakonadi/progresswidget/progressmanagerakonadi.cpp
@@ -0,0 +1,36 @@
+/*
+  progressmanager.cpp
+
+  Copyright (c) 2004 Till Adam <adam@kde.org>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
+*/
+
+#include "progressmanagerakonadi.h"
+#include "agentprogressmonitor.h"
+
+KPIM::ProgressItem *PimCommon::ProgressManagerAkonadi::createProgressItem(KPIM::ProgressItem *parent, const Akonadi::AgentInstance &agent, const QString &id, const QString &label, const QString &status, bool canBeCanceled, KPIM::ProgressItem::CryptoStatus cryptoStatus)
+{
+    const bool itemAlreadyExists = (KPIM::ProgressManager::instance()->progressItem(id) != nullptr);
+    KPIM::ProgressItem *t = KPIM::ProgressManager::instance()->createProgressItem(parent, id, label,
+                                                                                  status, canBeCanceled, cryptoStatus);
+    t->setTypeProgressItem(0);
+    if (!itemAlreadyExists) {
+        //    qCDebug(LIBKDEPIM_LOG) << "Created ProgressItem for agent" << instance.name();
+        new AgentProgressMonitor(agent, t);
+    }
+    return t;
+}
