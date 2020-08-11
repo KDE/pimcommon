e031c0d95944a3ae91167d4b14c9f2c16aba4594
Christophe Giboudeaux <christophe@krop.fr>
2020-08-11 22:11:28 +0200

    Convert license headers to SPDX format

--- src/pimcommonakonadi/addressline/addresslineedit/addresseelineedit.cpp
+++ src/pimcommonakonadi/addressline/addresslineedit/addresseelineedit.cpp
@@ -1,28 +1,15 @@
 /*
   This file is part of libkdepim.
 
-  Copyright (c) 2002 Helge Deller <deller@gmx.de>
-  Copyright (c) 2002 Lubos Lunak <llunak@suse.cz>
-  Copyright (c) 2001,2003 Carsten Pfeiffer <pfeiffer@kde.org>
-  Copyright (c) 2001 Waldo Bastian <bastian@kde.org>
-  Copyright (c) 2004 Daniel Molkentin <danimo@klaralvdalens-datakonsult.se>
-  Copyright (c) 2004 Karl-Heinz Zimmer <khz@klaralvdalens-datakonsult.se>
-  Copyright (C) 2017-2020 Laurent Montel <montel@kde.org>
+  SPDX-FileCopyrightText: 2002 Helge Deller <deller@gmx.de>
+  SPDX-FileCopyrightText: 2002 Lubos Lunak <llunak@suse.cz>
+  SPDX-FileCopyrightText: 2001, 2003 Carsten Pfeiffer <pfeiffer@kde.org>
+  SPDX-FileCopyrightText: 2001 Waldo Bastian <bastian@kde.org>
+  SPDX-FileCopyrightText: 2004 Daniel Molkentin <danimo@klaralvdalens-datakonsult.se>
+  SPDX-FileCopyrightText: 2004 Karl-Heinz Zimmer <khz@klaralvdalens-datakonsult.se>
+  SPDX-FileCopyrightText: 2017-2020 Laurent Montel <montel@kde.org>
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Library General Public
-  License as published by the Free Software Foundation; either
-  version 2 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Library General Public License for more details.
-
-  You should have received a copy of the GNU Library General Public License
-  along with this library; see the file COPYING.LIB.  If not, write to
-  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-  Boston, MA 02110-1301, USA.
+  SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #include "addresseelineedit.h"
