4f7bd4a2d8f4441258bda7cfb86ba3df3c6096a6
Laurent Montel <montel@kde.org>
2021-12-05 19:58:36 +0100

    GIT_SILENT: add support for CI reuse

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: none
+
 $EXTRACTRC `find . -name '*.ui'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libpimcommon.pot
 rm -f rc.cpp
