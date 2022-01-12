c35a9020f12744d1ae722a037708a2c4f4c0838d
Laurent Montel <montel@kde.org>
2022-01-12 19:37:55 +0100

    We depend against kf5.90

--- src/pimcommonakonadi/acl/aclutils.cpp
+++ src/pimcommonakonadi/acl/aclutils.cpp
@@ -8,37 +8,28 @@
 #include "aclutils_p.h"
 
 #include <ki18n_version.h>
-#if KI18N_VERSION >= QT_VERSION_CHECK(5, 89, 0)
 #include <KLazyLocalizedString>
-#undef I18NC_NOOP
-#define I18NC_NOOP kli18nc
-#endif
 #include <KLocalizedString>
 
 using namespace PimCommon;
 
 static const struct {
     KIMAP::Acl::Rights permissions;
-#if KI18N_VERSION >= QT_VERSION_CHECK(5, 89, 0)
     const KLazyLocalizedString userString;
-#else
-    const char *context;
-    const char *userString;
-#endif
 } standardPermissions[] = {
-    {KIMAP::Acl::None, I18NC_NOOP("Permissions", "None")},
+    {KIMAP::Acl::None, kli18nc("Permissions", "None")},
 
-    {KIMAP::Acl::Lookup | KIMAP::Acl::Read | KIMAP::Acl::KeepSeen, I18NC_NOOP("Permissions", "Read")},
+    {KIMAP::Acl::Lookup | KIMAP::Acl::Read | KIMAP::Acl::KeepSeen, kli18nc("Permissions", "Read")},
 
-    {KIMAP::Acl::Lookup | KIMAP::Acl::Read | KIMAP::Acl::KeepSeen | KIMAP::Acl::Insert | KIMAP::Acl::Post, I18NC_NOOP("Permissions", "Append")},
+    {KIMAP::Acl::Lookup | KIMAP::Acl::Read | KIMAP::Acl::KeepSeen | KIMAP::Acl::Insert | KIMAP::Acl::Post, kli18nc("Permissions", "Append")},
 
     {KIMAP::Acl::Lookup | KIMAP::Acl::Read | KIMAP::Acl::KeepSeen | KIMAP::Acl::Insert | KIMAP::Acl::Post | KIMAP::Acl::Write | KIMAP::Acl::CreateMailbox
          | KIMAP::Acl::DeleteMailbox | KIMAP::Acl::DeleteMessage | KIMAP::Acl::Expunge,
-     I18NC_NOOP("Permissions", "Write")},
+     kli18nc("Permissions", "Write")},
 
     {KIMAP::Acl::Lookup | KIMAP::Acl::Read | KIMAP::Acl::KeepSeen | KIMAP::Acl::Insert | KIMAP::Acl::Post | KIMAP::Acl::Write | KIMAP::Acl::CreateMailbox
          | KIMAP::Acl::DeleteMailbox | KIMAP::Acl::DeleteMessage | KIMAP::Acl::Expunge | KIMAP::Acl::Admin,
-     I18NC_NOOP("Permissions", "All")}};
+     kli18nc("Permissions", "All")}};
 
 uint AclUtils::standardPermissionsCount()
 {
@@ -69,11 +60,7 @@ QString AclUtils::permissionsToUserString(KIMAP::Acl::Rights permissions)
     const uint maxSize(sizeof(standardPermissions) / sizeof(*standardPermissions));
     for (uint i = 0; i < maxSize; ++i) {
         if (KIMAP::Acl::normalizedRights(permissions) == standardPermissions[i].permissions) {
-#if KI18N_VERSION >= QT_VERSION_CHECK(5, 89, 0)
             return KLocalizedString(standardPermissions[i].userString).toString();
-#else
-            return i18nc(standardPermissions[i].context, standardPermissions[i].userString);
-#endif
         }
     }
 
