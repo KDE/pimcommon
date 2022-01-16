de159c0b2825c79d47841524059d55aa0c306943
Laurent Montel <montel@kde.org>
2022-01-16 18:23:59 +0100

    Prepare to use QNetworkInformation when we use qt6

--- src/pimcommon/network/networkmanager.cpp
+++ src/pimcommon/network/networkmanager.cpp
@@ -5,7 +5,12 @@
 */
 
 #include "networkmanager.h"
+#include "pimcommon_debug.h"
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#include <QNetworkInformation>
+#else
 #include <QNetworkConfigurationManager>
+#endif
 using namespace PimCommon;
 
 Q_GLOBAL_STATIC(NetworkManager, s_pNetworkManagerSelf)
@@ -33,5 +38,14 @@ QNetworkConfigurationManager *NetworkManager::networkConfigureManager() const
 
 bool NetworkManager::isOnline() const
 {
+#if QT_VERSION >= QT_VERSION_CHECK(6, 1, 0)
+    if (QNetworkInformation::load(QNetworkInformation::Feature::Reachability)) {
+        return QNetworkInformation::instance()->reachability() == QNetworkInformation::Reachability::Online;
+    } else {
+        qCWarning(PIMCOMMON_LOG) << "Couldn't find a working backend for QNetworkInformation";
+        return false;
+    }
+#else
     return mNetworkConfigureManager->isOnline();
+#endif
 }
