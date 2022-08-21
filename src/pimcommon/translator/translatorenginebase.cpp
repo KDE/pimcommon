be9185d1585175551a9840cbf763a5e68c2603e7
Laurent Montel <montel@kde.org>
2022-08-21 10:19:13 +0200

    Start to create other translator engine

---
+++ src/pimcommon/translator/translatorenginebase.cpp
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "translatorenginebase.h"
+using namespace PimCommon;
+TranslatorEngineBase::TranslatorEngineBase(QObject *parent)
+    : QObject{parent}
+{
+}
+
+TranslatorEngineBase::~TranslatorEngineBase()
+{
+}
