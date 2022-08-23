5b8f1169dd3508d0d68882bc0507b965c404d1fb
Laurent Montel <montel@kde.org>
2022-08-23 13:29:34 +0200

    Add more autotest

---
+++ src/pimcommon/translator/autotests/translatorconfigurewidgettest.cpp
@@ -0,0 +1,33 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "translatorconfigurewidgettest.h"
+#include "translator/translatorconfigurewidget.h"
+#include <QComboBox>
+#include <QLabel>
+#include <QTest>
+#include <QVBoxLayout>
+QTEST_MAIN(TranslatorConfigureWidgetTest)
+TranslatorConfigureWidgetTest::TranslatorConfigureWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void TranslatorConfigureWidgetTest::shouldHaveDefaultValues()
+{
+    PimCommon::TranslatorConfigureWidget w;
+
+    auto mEngine = w.findChild<QComboBox *>(QStringLiteral("mEngine"));
+    QVERIFY(mEngine);
+
+    auto mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto label = w.findChild<QLabel *>(QStringLiteral("label"));
+    QVERIFY(label);
+    QVERIFY(!label->text().isEmpty());
+}
