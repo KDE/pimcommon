240b06d849c73bc844ea57b6119e51a9fe2406bf
Laurent Montel <montel@kde.org>
2022-08-25 21:17:36 +0200

    Continue to implement yandex translator

--- src/pimcommon/translator/engine/yandextranslator.cpp
+++ src/pimcommon/translator/engine/yandextranslator.cpp
@@ -7,7 +7,11 @@
 
 #include "yandextranslator.h"
 #include "translator/translatorengineaccessmanager.h"
+#include "translator/translatorutil.h"
 #include <KLocalizedString>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 using namespace PimCommon;
 QString YandexTranslator::sYandexKey;
@@ -38,7 +42,67 @@ void YandexTranslator::translate()
 
 QVector<QPair<QString, QString>> YandexTranslator::supportedLanguage() const
 {
-    return {};
+    TranslatorUtil translatorUtil;
+    QVector<QPair<QString, QString>> fullListLanguage;
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::automatic));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::en));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::nl));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::fr));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::de));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::el));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::it));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ja));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ko));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::pt));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ru));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::es));
+
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::af));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::sq));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ar));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::hy));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::az));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::eu));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::be));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::bg));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ca));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::hr));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::cs));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::da));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::et));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::tl));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::fi));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::gl));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ka));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ht));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::iw));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::hi));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::hu));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::is));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::id));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ga));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::lv));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::lt));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::mk));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ms));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::mt));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::no));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::fa));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::pl));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ro));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::sr));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::sk));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::sl));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::sw));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::sv));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::th));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::tr));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::uk));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::ur));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::vi));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::cy));
+    fullListLanguage.append(translatorUtil.pair(TranslatorUtil::yi));
+    return fullListLanguage;
 }
 
 void YandexTranslator::parseCredentials(QNetworkReply *reply)
@@ -52,7 +116,7 @@ void YandexTranslator::parseCredentials(QNetworkReply *reply)
                                     "from your computer network. Please try your request again later."));
         return;
     }
-
+    qDebug() << "webSiteData  " << webSiteData;
     const QByteArray sidBeginString = "SID: '";
     const int sidBeginStringPos = webSiteData.indexOf(sidBeginString);
 
@@ -92,40 +156,14 @@ void YandexTranslator::translateText()
 
     mResult.clear();
     QString lang;
-#if 0
-    const QString sourceText = sender()->property(s_textProperty).toString();
-
-    QString lang;
-
-    if (m_sourceLang == Auto)
-    {
-        lang = languageApiCode(Yandex, m_translationLang);
+    if (mFrom == QStringLiteral("auto")) {
+        lang = mTo;
+    } else {
+        lang = mFrom + QLatin1Char('-') + mTo;
     }
-    else
-    {
-        lang = languageApiCode(Yandex, m_sourceLang) + QLatin1Char('-') + languageApiCode(Yandex, m_translationLang);
-    }
-
+    // qDebug() << " lang " << lang;
     // Generate API url
     QUrl url(QStringLiteral("https://translate.yandex.net/api/v1/tr.json/translate"));
-
-    url.setQuery(QStringLiteral("id=%1-2-0&srv=tr-text&text=%2&lang=%3")
-                     .arg(s_yandexKey,
-                          QString::fromUtf8(QUrl::toPercentEncoding(sourceText)),
-                          lang));
-
-    // Setup request
-    QNetworkRequest request;
-    request.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("application/x-www-form-urlencoded"));
-    request.setUrl(url);
-
-    // Make reply
-    m_currentReply = m_networkManager->post(request, QByteArray());
-
-#endif
-    // Generate API url
-    QUrl url(QStringLiteral("https://translate.yandex.net/api/v1/tr.json/translate"));
-
     url.setQuery(QStringLiteral("id=%1-2-0&srv=tr-text&text=%2&lang=%3").arg(sYandexKey, QString::fromUtf8(QUrl::toPercentEncoding(mInputText)), lang));
 
     // Setup request
@@ -148,7 +186,27 @@ void YandexTranslator::translateText()
 
 void YandexTranslator::parseTranslation(QNetworkReply *reply)
 {
-    // TODO
+    const QJsonDocument jsonResponse = QJsonDocument::fromJson(reply->readAll());
+    const QJsonObject jsonData = jsonResponse.object();
     reply->deleteLater();
+
+    // Parse language
+    if (mFrom == QStringLiteral("auto")) {
+        QString sourceCode = jsonData.value(QStringLiteral("lang")).toString();
+        sourceCode = sourceCode.left(sourceCode.indexOf(QLatin1Char('-')));
+        // m_sourceLang       = language(Yandex, sourceCode);
+#if 0
+        if (m_sourceLang == NoLanguage)
+        {
+            Q_EMIT translateFailed(false i18n("Error: Unable to parse autodetected language"));
+            return;
+        }
+#endif
+    }
+
+    // Parse translation data
+    mResult += jsonData.value(QStringLiteral("text")).toArray().at(0).toString();
+    // qDebug() << " result " << reply->readAll();
+    // TODO
     Q_EMIT translateDone();
 }
