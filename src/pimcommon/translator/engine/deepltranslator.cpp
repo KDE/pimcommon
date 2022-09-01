906aa99f3363e240853c8e68c911cb539ca3d328
Laurent Montel <montel@kde.org>
2022-09-01 07:02:21 +0200

    Start to add deepltranslator

---
+++ src/pimcommon/translator/engine/deepltranslator.cpp
@@ -0,0 +1,77 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+  Code based on Digikam donlinetranslator
+*/
+
+#include "deepltranslator.h"
+#include "pimcommon_debug.h"
+#include "translator/translatorengineaccessmanager.h"
+#include "translator/translatorutil.h"
+#include <KLocalizedString>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonParseError>
+#include <QNetworkAccessManager>
+
+using namespace PimCommon;
+DeepLTranslator::DeepLTranslator(QObject *parent)
+    : TranslatorEngineBase{parent}
+{
+}
+
+DeepLTranslator::~DeepLTranslator() = default;
+
+QString PimCommon::DeepLTranslator::engineName() const
+{
+    return i18n("DeepL");
+}
+
+void DeepLTranslator::translate()
+{
+    if (mFrom == mTo) {
+        Q_EMIT translateFailed(false, i18n("You used same language for from and to language."));
+        return;
+    }
+    translateText();
+}
+
+QVector<QPair<QString, QString>> DeepLTranslator::supportedLanguage() const
+{
+    return {};
+}
+
+void DeepLTranslator::translateText()
+{
+    mResult.clear();
+
+    const QUrl url(QString::fromUtf8("%1/api/v1/%2/%3/%4").arg(mServerUrl).arg(mFrom).arg(mTo).arg(QString::fromUtf8(QUrl::toPercentEncoding(mInputText))));
+
+    const QNetworkRequest request(url);
+
+    QNetworkReply *reply = TranslatorEngineAccessManager::self()->networkManager()->get(request);
+    connect(reply, &QNetworkReply::errorOccurred, this, [this, reply](QNetworkReply::NetworkError error) {
+        slotError(error);
+        reply->deleteLater();
+    });
+    connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        reply->deleteLater();
+        parseTranslation(reply);
+    });
+}
+
+void DeepLTranslator::parseTranslation(QNetworkReply *reply)
+{
+    if (reply->error() != QNetworkReply::NoError) {
+        Q_EMIT translateFailed(false, reply->errorString());
+        reply->deleteLater();
+        return;
+    }
+
+    const QJsonDocument jsonResponse = QJsonDocument::fromJson(reply->readAll());
+    const QJsonObject responseObject = jsonResponse.object();
+    mResult = responseObject.value(QStringLiteral("translation")).toString();
+    reply->deleteLater();
+    Q_EMIT translateDone();
+}
---
+++ src/pimcommon/translator/engine/deepltranslator.h
@@ -0,0 +1,28 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+  Code based on Digikam donlinetranslator
+*/
+
+#pragma once
+
+#include "translator/translatorenginebase.h"
+namespace PimCommon
+{
+class DeepLTranslator : public TranslatorEngineBase
+{
+    Q_OBJECT
+public:
+    explicit DeepLTranslator(QObject *parent = nullptr);
+    ~DeepLTranslator() override;
+
+    Q_REQUIRED_RESULT QString engineName() const override;
+    void translate() override;
+    Q_REQUIRED_RESULT QVector<QPair<QString, QString>> supportedLanguage() const override;
+
+private:
+    void translateText();
+    void parseTranslation(QNetworkReply *reply);
+};
+}
