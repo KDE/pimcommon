03d84a03390447b522e200d7cfbfced734a55c76
Laurent Montel <montel@kde.org>
2022-10-24 13:07:36 +0200

    Prepare to separate setting from autocorrection methods

---
+++ src/pimcommon/autocorrection/autocorrectionsettings.cpp
@@ -0,0 +1,13 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "autocorrectionsettings.h"
+using namespace PimCommon;
+AutoCorrectionSettings::AutoCorrectionSettings()
+{
+}
+
+AutoCorrectionSettings::~AutoCorrectionSettings() = default;
---
+++ src/pimcommon/autocorrection/autocorrectionsettings.h
@@ -0,0 +1,17 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+#include "pimcommon_export.h"
+namespace PimCommon
+{
+class PIMCOMMON_EXPORT AutoCorrectionSettings
+{
+public:
+    AutoCorrectionSettings();
+    ~AutoCorrectionSettings();
+};
+}
