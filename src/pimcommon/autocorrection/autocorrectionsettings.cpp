7d3b91eeab2100b270acdcfbb55297e4916c5f76
Laurent Montel <montel@kde.org>
2022-10-27 13:49:49 +0200

    Remove duplicate code

--- src/pimcommon/autocorrection/autocorrectionsettings.cpp
+++ src/pimcommon/autocorrection/autocorrectionsettings.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "autocorrectionsettings.h"
+#include "autocorrection/export/exportkmailautocorrection.h"
 #include "autocorrection/export/exportlibreofficeautocorrection.h"
 #include "autocorrection/import/importkmailautocorrection.h"
 #include "autocorrection/import/importlibreofficeautocorrection.h"
@@ -333,73 +334,14 @@ void AutoCorrectionSettings::writeAutoCorrectionXmlFile(const QString &filename)
         qCDebug(PIMCOMMON_LOG) << "We can't save in file :" << filename;
     }
 #else
-    const QString fname = filename.isEmpty() ? QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + QLatin1String("/autocorrect/custom-")
-            + (mAutoCorrectLang == QLatin1String("en_US") ? QStringLiteral("autocorrect") : mAutoCorrectLang) + QLatin1String(".xml")
-                                             : filename;
-    QFileInfo fileInfo(fname);
-    QDir().mkpath(fileInfo.absolutePath());
-    QFile file(fname);
-    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-        qCDebug(PIMCOMMON_LOG) << "We can't save in file :" << fname;
-        return;
+    ExportKMailAutocorrection correct;
+    correct.setAutocorrectEntries(mAutocorrectEntries);
+    correct.setUpperCaseExceptions(mUpperCaseExceptions);
+    correct.setTwoUpperLetterExceptions(mTwoUpperLetterExceptions);
+    QString message;
+    if (!correct.exportData(mAutoCorrectLang, filename, message)) {
+        qCDebug(PIMCOMMON_LOG) << "We can't save in file :" << filename;
     }
-
-    QXmlStreamWriter streamWriter(&file);
-
-    streamWriter.setAutoFormatting(true);
-    streamWriter.setAutoFormattingIndent(2);
-    streamWriter.writeStartDocument();
-
-    streamWriter.writeDTD(QStringLiteral("<!DOCTYPE autocorrection>"));
-
-    streamWriter.writeStartElement(QStringLiteral("Word"));
-
-    streamWriter.writeStartElement(QStringLiteral("items"));
-    QHashIterator<QString, QString> i(mAutocorrectEntries);
-    while (i.hasNext()) {
-        i.next();
-        streamWriter.writeStartElement(QStringLiteral("item"));
-        streamWriter.writeAttribute(QStringLiteral("find"), i.key());
-        streamWriter.writeAttribute(QStringLiteral("replace"), i.value());
-        streamWriter.writeEndElement();
-    }
-    streamWriter.writeEndElement();
-
-    streamWriter.writeStartElement(QStringLiteral("UpperCaseExceptions"));
-    QSet<QString>::const_iterator upper = mUpperCaseExceptions.constBegin();
-    while (upper != mUpperCaseExceptions.constEnd()) {
-        streamWriter.writeStartElement(QStringLiteral("word"));
-        streamWriter.writeAttribute(QStringLiteral("exception"), *upper);
-        ++upper;
-        streamWriter.writeEndElement();
-    }
-    streamWriter.writeEndElement();
-
-    streamWriter.writeStartElement(QStringLiteral("TwoUpperLetterExceptions"));
-    QSet<QString>::const_iterator twoUpper = mTwoUpperLetterExceptions.constBegin();
-    while (twoUpper != mTwoUpperLetterExceptions.constEnd()) {
-        streamWriter.writeStartElement(QStringLiteral("word"));
-        streamWriter.writeAttribute(QStringLiteral("exception"), *twoUpper);
-        ++twoUpper;
-        streamWriter.writeEndElement();
-    }
-    streamWriter.writeEndElement();
-
-    streamWriter.writeStartElement(QStringLiteral("DoubleQuote"));
-    streamWriter.writeStartElement(QStringLiteral("doublequote"));
-    streamWriter.writeAttribute(QStringLiteral("begin"), mTypographicDoubleQuotes.begin);
-    streamWriter.writeAttribute(QStringLiteral("end"), mTypographicDoubleQuotes.end);
-    streamWriter.writeEndElement();
-    streamWriter.writeEndElement();
-
-    streamWriter.writeStartElement(QStringLiteral("SimpleQuote"));
-    streamWriter.writeStartElement(QStringLiteral("simplequote"));
-    streamWriter.writeAttribute(QStringLiteral("begin"), mTypographicSingleQuotes.begin);
-    streamWriter.writeAttribute(QStringLiteral("end"), mTypographicSingleQuotes.end);
-    streamWriter.writeEndElement();
-    streamWriter.writeEndElement();
-
-    streamWriter.writeEndDocument();
 #endif
 }
 
