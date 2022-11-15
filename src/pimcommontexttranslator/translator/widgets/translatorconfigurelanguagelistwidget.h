a2fd035922ba4602e3ffefe84fbefd07296d2eef
Laurent Montel <montel@kde.org>
2022-11-15 13:45:52 +0100

    Add some class from ruqola

---
+++ src/pimcommontexttranslator/translator/widgets/translatorconfigurelanguagelistwidget.h
@@ -0,0 +1,37 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QWidget>
+class QListWidget;
+class KListWidgetSearchLine;
+class QLabel;
+namespace PimCommonTextTranslator
+{
+class TranslatorConfigureLanguageListWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit TranslatorConfigureLanguageListWidget(const QString &labelText, QWidget *parent = nullptr);
+    ~TranslatorConfigureLanguageListWidget() override;
+
+    void clear();
+
+    void addItem(const QPair<QString, QString> &lang);
+
+    Q_REQUIRED_RESULT QStringList selectedLanguages() const;
+    void setSelectedLanguages(const QStringList &list);
+
+private:
+    enum LanguageData {
+        LanguageCode = Qt::UserRole + 1,
+    };
+    QListWidget *const mLanguageListWidget;
+    KListWidgetSearchLine *const mListSearchLine;
+    QLabel *const mLabel;
+};
+}
