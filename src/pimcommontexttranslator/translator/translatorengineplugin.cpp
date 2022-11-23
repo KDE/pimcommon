bd2864fc1f086f1dfcb0fbaa2852262a8cae434c
Laurent Montel <montel@kde.org>
2022-11-23 20:27:17 +0100

    Prepare to create plugins

---
+++ src/pimcommontexttranslator/translator/translatorengineplugin.cpp
@@ -0,0 +1,14 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "translatorengineplugin.h"
+using namespace PimCommonTextTranslator;
+
+TranslatorEnginePlugin::TranslatorEnginePlugin()
+{
+}
+
+TranslatorEnginePlugin::~TranslatorEnginePlugin() = default;
