b93dbcffe80dabd14d87bdd343b35611dee886c9
Laurent Montel <montel@kde.org>
2022-11-23 23:00:19 +0100

    Continue to implement googleengineplugin

---
+++ src/pimcommontexttranslator/translator/plugins/google/googleengineplugin.cpp
@@ -0,0 +1,19 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "googleengineplugin.h"
+
+GoogleEnginePlugin::GoogleEnginePlugin()
+    : PimCommonTextTranslator::TranslatorEnginePlugin()
+{
+}
+
+GoogleEnginePlugin::~GoogleEnginePlugin() = default;
+
+void GoogleEnginePlugin::translate()
+{
+    // TODO
+}
