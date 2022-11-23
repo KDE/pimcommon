67624b394dfa8a2504f2a7187c9fd88acd0e8c4f
Laurent Montel <montel@kde.org>
2022-11-23 22:14:46 +0100

    Add more api

--- src/pimcommontexttranslator/translator/translatorengineplugin.cpp
+++ src/pimcommontexttranslator/translator/translatorengineplugin.cpp
@@ -7,8 +7,47 @@
 #include "translatorengineplugin.h"
 using namespace PimCommonTextTranslator;
 
+class PimCommonTextTranslator::TranslatorEnginePluginPrivate
+{
+public:
+    QString mText;
+    QString mFrom;
+    QString mTo;
+};
+
 TranslatorEnginePlugin::TranslatorEnginePlugin()
+    : d(new PimCommonTextTranslator::TranslatorEnginePluginPrivate)
 {
 }
 
 TranslatorEnginePlugin::~TranslatorEnginePlugin() = default;
+
+void TranslatorEnginePlugin::setInputText(const QString &text)
+{
+    d->mText = text;
+}
+
+void TranslatorEnginePlugin::setFrom(const QString &language)
+{
+    d->mFrom = language;
+}
+
+void TranslatorEnginePlugin::setTo(const QString &language)
+{
+    d->mTo = language;
+}
+
+QString TranslatorEnginePlugin::inputText() const
+{
+    return d->mText;
+}
+
+QString TranslatorEnginePlugin::from() const
+{
+    return d->mFrom;
+}
+
+QString TranslatorEnginePlugin::to() const
+{
+    return d->mTo;
+}
--- src/pimcommontexttranslator/translator/translatorengineplugin.h
+++ src/pimcommontexttranslator/translator/translatorengineplugin.h
@@ -7,12 +7,30 @@
 #pragma once
 
 #include "pimcommontexttranslator_export.h"
+#include <QString>
+#include <memory>
+
 namespace PimCommonTextTranslator
 {
+class TranslatorEnginePluginPrivate;
 class PIMCOMMONTEXTTRANSLATOR_EXPORT TranslatorEnginePlugin
 {
 public:
     TranslatorEnginePlugin();
-    ~TranslatorEnginePlugin();
+    virtual ~TranslatorEnginePlugin();
+
+    virtual void translate() = 0;
+
+    Q_REQUIRED_RESULT QString resultTranslate() const;
+    void setInputText(const QString &text);
+    void setFrom(const QString &language);
+    void setTo(const QString &language);
+
+    Q_REQUIRED_RESULT QString inputText() const;
+    Q_REQUIRED_RESULT QString from() const;
+    Q_REQUIRED_RESULT QString to() const;
+
+private:
+    std::unique_ptr<TranslatorEnginePluginPrivate> const d;
 };
 }
