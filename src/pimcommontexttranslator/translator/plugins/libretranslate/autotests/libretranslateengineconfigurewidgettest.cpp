0c6305bd08828380ce0e2d01bc70f62a736a1793
Laurent Montel <montel@kde.org>
2022-12-05 13:52:25 +0100

    Start to add libretranslateengineconfigurewidget autotest

---
+++ src/pimcommontexttranslator/translator/plugins/libretranslate/autotests/libretranslateengineconfigurewidgettest.cpp
@@ -0,0 +1,13 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "libretranslateengineconfigurewidgettest.h"
+#include <QTest>
+QTEST_MAIN(LibreTranslateEngineConfigureWidgetTest)
+LibreTranslateEngineConfigureWidgetTest::LibreTranslateEngineConfigureWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
