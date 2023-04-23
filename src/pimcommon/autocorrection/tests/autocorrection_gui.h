6d164817690b6f6c95e3628b7bee87639ae4e0fc
Laurent Montel <montel@kde.org>
2023-04-23 14:59:50 +0200

    Adapt to new autocorrection lib (split core/widgets)

--- src/pimcommon/autocorrection/tests/autocorrection_gui.h
+++ src/pimcommon/autocorrection/tests/autocorrection_gui.h
@@ -10,12 +10,24 @@
 #include <QDialog>
 #include <QWidget>
 
+#include "config-pimcommon.h"
 #include <QTextEdit>
+#if HAVE_TEXT_AUTOCORRECTION_WIDGETS
+namespace TextAutoCorrectionCore
+{
+class AutoCorrection;
+}
+namespace TextAutoCorrectionWidgets
+{
+class AutoCorrectionWidget;
+}
+#else
 namespace TextAutoCorrection
 {
 class AutoCorrection;
 class AutoCorrectionWidget;
 }
+#endif
 namespace PimCommon
 {
 class LineEditWithAutoCorrection;
@@ -24,28 +36,44 @@ class ConfigureTestDialog : public QDialog
 {
     Q_OBJECT
 public:
+#if HAVE_TEXT_AUTOCORRECTION_WIDGETS
+    explicit ConfigureTestDialog(TextAutoCorrectionCore::AutoCorrection *autoCorrection, QWidget *parent = nullptr);
+#else
     explicit ConfigureTestDialog(TextAutoCorrection::AutoCorrection *autoCorrection, QWidget *parent = nullptr);
+#endif
     ~ConfigureTestDialog() override;
 
 private Q_SLOTS:
     void slotSaveSettings();
 
 private:
+#if HAVE_TEXT_AUTOCORRECTION_WIDGETS
+    TextAutoCorrectionWidgets::AutoCorrectionWidget *mWidget = nullptr;
+#else
     TextAutoCorrection::AutoCorrectionWidget *mWidget = nullptr;
+#endif
 };
 
 class TextEditAutoCorrectionWidget : public QTextEdit
 {
     Q_OBJECT
 public:
+#if HAVE_TEXT_AUTOCORRECTION_WIDGETS
+    explicit TextEditAutoCorrectionWidget(TextAutoCorrectionCore::AutoCorrection *autoCorrection, QWidget *parent = nullptr);
+#else
     explicit TextEditAutoCorrectionWidget(TextAutoCorrection::AutoCorrection *autoCorrection, QWidget *parent = nullptr);
+#endif
     ~TextEditAutoCorrectionWidget() override;
 
 protected:
     void keyPressEvent(QKeyEvent *e) override;
 
 private:
+#if HAVE_TEXT_AUTOCORRECTION_WIDGETS
+    TextAutoCorrectionCore::AutoCorrection *mAutoCorrection = nullptr;
+#else
     TextAutoCorrection::AutoCorrection *mAutoCorrection = nullptr;
+#endif
 };
 
 class AutocorrectionTestWidget : public QWidget
@@ -62,6 +90,10 @@ private Q_SLOTS:
 private:
     TextEditAutoCorrectionWidget *mEdit = nullptr;
     PimCommon::LineEditWithAutoCorrection *mSubject = nullptr;
+#if HAVE_TEXT_AUTOCORRECTION_WIDGETS
+    TextAutoCorrectionCore::AutoCorrection *mAutoCorrection = nullptr;
+#else
     TextAutoCorrection::AutoCorrection *mAutoCorrection = nullptr;
+#endif
     KSharedConfig::Ptr mConfig;
 };
