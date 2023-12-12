f76a287781ccfee091b31be086ae475e9a71b585
Laurent Montel <montel@kde.org>
2023-12-12 13:32:13 +0100

    Use PimCommon::NeedUpdateVersionUtils::checkVersion() + hide widget

--- src/pimcommon/needupdateversion/needupdateversionwidget.cpp
+++ src/pimcommon/needupdateversion/needupdateversionwidget.cpp
@@ -23,29 +23,32 @@ NeedUpdateVersionWidget::~NeedUpdateVersionWidget() = default;
 
 void NeedUpdateVersionWidget::setObsoleteVersion(NeedUpdateVersionUtils::ObsoleteVersion obsolete)
 {
-    switch (obsolete) {
-    case NeedUpdateVersionUtils::ObsoleteVersion::Unknown:
-        qCWarning(PIMCOMMON_LOG) << "Unknown value: seems to be a bug";
-        break;
-    case NeedUpdateVersionUtils::ObsoleteVersion::OlderThan6Months:
-        setMessageType(KMessageWidget::Information);
-        setText(i18n("Your version is older than 6 months, we encourage you to upgrade"));
-        animatedShow();
-        break;
-    case NeedUpdateVersionUtils::ObsoleteVersion::OlderThan12Months:
-        setMessageType(KMessageWidget::Warning);
-        setText(i18n("Your version is older than 1 year, we strongly encourage you to upgrade"));
-        animatedShow();
-        break;
-    case NeedUpdateVersionUtils::ObsoleteVersion::NotObsoleteYet:
-        // It's ok
-        break;
+    if (PimCommon::NeedUpdateVersionUtils::checkVersion()) {
+        switch (obsolete) {
+        case NeedUpdateVersionUtils::ObsoleteVersion::Unknown:
+            qCWarning(PIMCOMMON_LOG) << "Unknown value: seems to be a bug";
+            break;
+        case NeedUpdateVersionUtils::ObsoleteVersion::OlderThan6Months:
+            setMessageType(KMessageWidget::Information);
+            setText(i18n("Your version is older than 6 months, we encourage you to upgrade"));
+            animatedShow();
+            break;
+        case NeedUpdateVersionUtils::ObsoleteVersion::OlderThan12Months:
+            setMessageType(KMessageWidget::Warning);
+            setText(i18n("Your version is older than 1 year, we strongly encourage you to upgrade"));
+            animatedShow();
+            break;
+        case NeedUpdateVersionUtils::ObsoleteVersion::NotObsoleteYet:
+            // It's ok
+            break;
+        }
     }
 }
 
 void NeedUpdateVersionWidget::slotDisableVersionCheck()
 {
     PimCommon::NeedUpdateVersionUtils::disableCheckVersion();
+    animatedHide();
 }
 
 #include "moc_needupdateversionwidget.cpp"
