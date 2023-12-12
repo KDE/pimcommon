34fb9c59f2fdf75f9c1bf071902c909090c5c34a
Laurent Montel <montel@kde.org>
2023-12-12 08:32:03 +0100

    Prepare util class

---
+++ src/pimcommon/needupdateversion/needupdateversionutils.h
@@ -0,0 +1,13 @@
+/*
+  SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+class NeedUpdateVersionUtils
+{
+public:
+    NeedUpdateVersionUtils();
+};
