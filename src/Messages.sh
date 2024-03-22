33e2a6feeeedb986ecd326b708dc1b232906460d
Laurent Montel <montel@kde.org>
2024-03-22 13:37:06 +0100

    There is not ui file

--- src/Messages.sh
+++ src/Messages.sh
@@ -2,6 +2,4 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: none
 
-$EXTRACTRC `find . -name '*.ui'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libpimcommon6.pot
-rm -f rc.cpp
