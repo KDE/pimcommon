cf4bdd03ab2c7cf6f642ba9d79feb0e1160e1e90
Laurent Montel <montel@kde.org>
2024-06-03 07:18:27 +0200

    Check isBehindCaptivePortal here too

--- src/pimcommonakonadi/addressline/addresslineedit/addresseelineeditmanager.cpp
+++ src/pimcommonakonadi/addressline/addresslineedit/addresseelineeditmanager.cpp
@@ -114,7 +114,8 @@ void AddresseeLineEditManager::setAddressLineEdit(AddresseeLineEdit *addressLine
 bool AddresseeLineEditManager::isOnline() const
 {
     if (QNetworkInformation::loadBackendByFeatures(QNetworkInformation::Feature::Reachability)) {
-        return QNetworkInformation::instance()->reachability() == QNetworkInformation::Reachability::Online;
+        return QNetworkInformation::instance()->reachability() == QNetworkInformation::Reachability::Online
+            && !QNetworkInformation::instance()->isBehindCaptivePortal();
     } else {
         qCWarning(PIMCOMMONAKONADI_LOG) << "Couldn't find a working backend for QNetworkInformation";
         return false;
