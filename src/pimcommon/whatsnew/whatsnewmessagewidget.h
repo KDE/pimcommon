e842c1060781839b9a30c83d7b35e6d902b008af
Laurent Montel <montel@kde.org>
2024-08-05 07:48:45 +0200

    Install WhatsNewMessageWidget

--- src/pimcommon/whatsnew/whatsnewmessagewidget.h
+++ src/pimcommon/whatsnew/whatsnewmessagewidget.h
@@ -4,11 +4,11 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #pragma once
-#include "pimcommon_private_export.h"
+#include "pimcommon_export.h"
 #include <KMessageWidget>
 namespace PimCommon
 {
-class PIMCOMMON_TESTS_EXPORT WhatsNewMessageWidget : public KMessageWidget
+class PIMCOMMON_EXPORT WhatsNewMessageWidget : public KMessageWidget
 {
     Q_OBJECT
 public:
