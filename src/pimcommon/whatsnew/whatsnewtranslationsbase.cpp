89dabd2eec9fa2fba462dd5418e034c2a634ac58
Laurent Montel <montel@kde.org>
2024-08-13 07:35:07 +0200

    Remove duplicate includes

--- src/pimcommon/whatsnew/whatsnewtranslationsbase.cpp
+++ src/pimcommon/whatsnew/whatsnewtranslationsbase.cpp
@@ -4,7 +4,6 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "whatsnewtranslationsbase.h"
-#include <KLazyLocalizedString>
 #include <QCryptographicHash>
 using namespace PimCommon;
 WhatsNewTranslationsBase::WhatsNewTranslationsBase() = default;
